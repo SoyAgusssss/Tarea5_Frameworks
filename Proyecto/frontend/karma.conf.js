ec8f8d1c79ec330782226bad840e14627823a3e5
Miguel_Agustin <miguel-agus1995@hotmail.com>
2026-04-12 22:43:58 +0200

    cambios en el frontend

--- Proyecto/frontend/karma.conf.js
+++ Proyecto/frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/proyecto-ut2'),
+      dir: require('path').join(__dirname, './coverage/proyecto-ut5'),
       subdir: '.',
       reporters: [
         { type: 'html' },
